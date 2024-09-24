a7847eef62dd3084329fc8c83803fec59aaa570a
AdamullaOsas <kpasek22@gmail.com>
2024-09-24 23:09:17 +0200

    end for today

--- bookmark_landing_page/src/App.jsx
+++ bookmark_landing_page/src/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+import React, { useEffect, useState } from "react";
+import MobileNavbar from "./components/MobileNavbar";
 import BookmarkManager from "./components/BookmarkManager";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 const App = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <div className="w-full min-h-screen flex flex-col">
-            <Navbar />
+            {isMobile ? <MobileNavbar /> : <Navbar />}
             <BookmarkManager />
+            <Features />
         </div>
     );
 };
