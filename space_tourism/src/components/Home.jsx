5c218d6ff4f070e083b97bdc880a0af464be42b3
AdamullaOsas <kpasek22@gmail.com>
2024-09-29 14:30:29 +0200

    jade babcie zawiesc

--- space_tourism/src/components/Home.jsx
+++ space_tourism/src/components/Home.jsx
@@ -19,12 +19,27 @@ const Home = () => {
 
     return (
         <div
-            className="min-h-screen w-full bg-center text-white p-6"
+            className="min-h-screen w-full bg-center p-6 flex flex-col items-center"
             style={{
                 backgroundImage: `url(${backgroundImage})`,
                 backgroundSize: "cover",
             }}
-        ></div>
+        >
+            <div className="flex flex-col gap-6 items-center text-center max-w-[512px] xl:max-w-[540px]">
+                <p className="textPreset6Mobile text-blue-300 md:textPreset5">
+                    SO, YOU WANT TO TRAVEL TO
+                </p>
+                <h1 className="textPreset1Mobile text-white md:textPreset1">
+                    SPACE
+                </h1>
+                <p className="textPreset9Mobile md:textPreset9 text-blue-300">
+                    Let's face it; if you want to go to space, you might as well
+                    genuinely go to outer space and not hover kind of on the
+                    edge of it. Well sit back, and relax because we'll give you
+                    a truly out of this world experience!
+                </p>
+            </div>
+        </div>
     );
 };
 
