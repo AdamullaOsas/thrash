6bfde7acf4463d4cac4f4178c6b5dbe19ecf8ca3
AdamullaOsas <kpasek22@gmail.com>
2024-09-29 23:17:37 +0200

    crew finished esss

--- space_tourism/src/components/Crew.jsx
+++ space_tourism/src/components/Crew.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import data from "../data/data.json";
 
 import member1 from "../assets/crew/image-douglas-hurley.webp";
 import member2 from "../assets/crew/image-mark-shuttleworth.webp";
@@ -6,10 +8,53 @@ import member3 from "../assets/crew/image-victor-glover.webp";
 import member4 from "../assets/crew/image-anousheh-ansari.webp";
 
 const Crew = () => {
+    const { crew } = data;
+    const [active, setActive] = useState(0);
     const members = [member1, member2, member3, member4];
     return (
-        <div className="min-h-screen w-full flex flex-col items-center max-w-[1110px] xl:mx-auto p-6 md:p-10 xl:p-0 xl:py-12 pt-[112px] md:pt-[128px] xl:pt-[184px]">
-            Crew
+        <div className="min-h-screen w-full flex flex-col items-center xl:items-start max-w-[1110px] xl:mx-auto p-6 md:pb-0 md:p-10 xl:p-0 xl:py-12 pt-[112px] md:pt-[128px] xl:pt-[184px] xl:flex-row xl:max-w-[1110px] xl:justify-between">
+            <div className="xl:flex xl:flex-col xl:justify-between xl:h-[630px]">
+                <div className="flex gap-6 font-BarlowC text-white md:self-start ">
+                    <span className="opacity-25 font-bold textPreset6Mobile md:text-[20px] xl:text-[28px]">
+                        02
+                    </span>
+                    <p className="textPreset6Mobile md:text-[20px] xl:text-[28px]">
+                        MEET YOUR CREW
+                    </p>
+                </div>
+                <div className="flex flex-col mt-16 text-center xl:text-start md:max-w-[512px] xl:max-w-[540px]">
+                    <div className="flex flex-col gap-2">
+                        <p className="textPreset4Mobile md:text-[28px] text-white opacity-50 xl:text-[32px] uppercase">
+                            {crew[active].role}
+                        </p>
+                        <p className="textPreset3Mobile md:text-[40px] text-white xl:text-[56px] uppercase">
+                            {crew[active].name}
+                        </p>
+                    </div>
+                    <p className="textPreset9Mobile text-blue-300 md:text-[16px] xl:text-[18px] h-[164px] md:h-[117px]">
+                        {crew[active].bio}
+                    </p>
+                </div>
+                <div className="flex gap-4 xl:gap-10 items-center justify-center mt-6 xl:mt-0 xl:mb-12 xl:justify-self-end xl:self-start">
+                    {members.map((member, index) => (
+                        <button
+                            onClick={() => setActive(index)}
+                            className={`size-[10px] xl:size-[15px] rounded-full bg-white ${
+                                active === index
+                                    ? "bg-opacity-100"
+                                    : "bg-opacity-[17.44%]"
+                            }`}
+                        ></button>
+                    ))}
+                </div>
+            </div>
+            <div className="px-[28px] py-[5px] md:p-0 ">
+                <img
+                    src={members[active]}
+                    alt="crew member"
+                    className="mt-8 h-[340px] md:mx-auto md:h-[560px] xl:h-[600px] xl:mt-0"
+                />
+            </div>
         </div>
     );
 };
--- space_tourism/src/components/Destination.jsx
+++ space_tourism/src/components/Destination.jsx
@@ -14,42 +14,55 @@ const Destination = () => {
 
     return (
         <div className="min-h-screen w-full flex flex-col items-center max-w-[1110px] xl:mx-auto p-6 md:p-10 xl:p-0 xl:py-12 pt-[112px] md:pt-[128px] xl:pt-[184px]">
-            <div className="flex gap-6 font-BarlowC text-white">
-                <span className="opacity-25 font-bold textPreset6Mobile">
+            <div className="flex gap-6 font-BarlowC text-white md:self-start">
+                <span className="opacity-25 font-bold textPreset6Mobile md:text-[20px] xl:text-[28px]">
                     01
                 </span>
-                <p className="textPreset6Mobile">PICK YOUR DESTINATION</p>
+                <p className="textPreset6Mobile md:text-[20px] xl:text-[28px]">
+                    PICK YOUR DESTINATION
+                </p>
             </div>
-            <div className="flex flex-col items-center pb-6 border-b-[1px] border-blue-300">
+            <div className="flex flex-col xl:flex-row items-center xl:w-full xl:justify-between">
                 <img
                     src={images[active]}
                     alt="Destination"
                     className="size-[150px] md:size-[300px] xl:size-[480px] mt-[50px] md:mt-[66px] xd:mt-0"
                 />
-                <div className="flex gap-8 mt-[58px] textPreset8Mobile text-blue-300">
-                    {links.map((link, index) => (
-                        <button onClick={() => setActive(index)}>{link}</button>
-                    ))}
-                </div>
-                <h1 className="textPreset2Mobile text-white mt-6">
-                    {destinations[active].name}
-                </h1>
-                <p className="textPreset9Mobile text-blue-300 text-center">
-                    {destinations[active].description}
-                </p>
-            </div>
-            <div className="mt-6 flex flex-col justify-between items-center">
-                <div className="flex flex-col items-center gap-3">
-                    <p className="textPreset7">AVG. DISTANCE</p>
-                    <p className="textPreset6 uppercase">
-                        {destinations[active].distance}
-                    </p>
-                </div>
-                <div className="flex flex-col items-center gap-3">
-                    <p className="textPreset7 uppercase">Est. travel time</p>
-                    <p className="textPreset6 uppercase">
-                        {destinations[active].travel}
-                    </p>
+                <div className="flex flex-col xl:mt-[133px]">
+                    <div className="flex flex-col items-center xl:items-start pb-6 xl:pb-10 border-b-[1px] border-blue-300 xl:max-w-[445px]">
+                        <div className="flex gap-8 mt-[58px] md:mt-[74px] xl:mt-0 textPreset8Mobile md:textPreset8 text-blue-300">
+                            {links.map((link, index) => (
+                                <button onClick={() => setActive(index)}>
+                                    {link}
+                                </button>
+                            ))}
+                        </div>
+                        <h1 className="textPreset2Mobile md:text-[80px] text-white mt-6">
+                            {destinations[active].name}
+                        </h1>
+                        <p className="textPreset9Mobile text-blue-300 md:max-w-[514px] text-center xl:text-start">
+                            {destinations[active].description}
+                        </p>
+                    </div>
+
+                    <div className="mt-6 xl:mt-10 flex flex-col justify-between items-center md:flex-row md:w-full md:max-w-[514px] gap-6">
+                        <div className="flex flex-col items-center xl:items-start gap-3 md:w-1/2">
+                            <p className="textPreset7 text-blue-300">
+                                AVG. DISTANCE
+                            </p>
+                            <p className="textPreset6 uppercase">
+                                {destinations[active].distance}
+                            </p>
+                        </div>
+                        <div className="flex flex-col items-center xl:items-start gap-3 md:w-1/2">
+                            <p className="textPreset7 uppercase text-blue-300">
+                                Est. travel time
+                            </p>
+                            <p className="textPreset6 uppercase">
+                                {destinations[active].travel}
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
