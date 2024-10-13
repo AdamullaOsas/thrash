fc2d1f984b33cd22df74b53d021391339a098be3
AdamullaOsas <kpasek22@gmail.com>
2024-10-14 00:05:11 +0200

    ide spac nara

--- memory/src/components/Single.jsx
+++ memory/src/components/Single.jsx
@@ -4,10 +4,26 @@ import { useLocation } from "react-router-dom";
 const Single = () => {
     const location = useLocation();
     const { theme, gridSize } = location.state;
+
+    const arr = Array.from({ length: gridSize * gridSize }, (_, i) => i + 1);
+
     return (
-        <div>
-            {theme}
-            {gridSize}
+        <div className="flex flex-col p-6 w-full min-h-screen">
+            <div className="flex justify-between items-center mb-20">
+                <img src="/logo.svg" alt="" className="w-[92px]" />
+                <button className="h-10 w-[78px] bg-[#FDA214] text-[#FCFCFC] rounded-[26px] startButtons">
+                    Menu
+                </button>
+            </div>
+            <div
+                className={`grid ${
+                    gridSize === 4 ? "grid-cols-4" : "grid-cols-6"
+                }`}
+            >
+                {arr.map((item) => (
+                    <div>hi</div>
+                ))}
+            </div>
         </div>
     );
 };
