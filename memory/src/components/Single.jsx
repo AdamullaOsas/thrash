92142d1f1e7d05a99bd3ac80423f7e6b633d4cd7
AdamullaOsas <kpasek22@gmail.com>
2024-10-16 12:16:28 +0200

    single finished

--- memory/src/components/Single.jsx
+++ memory/src/components/Single.jsx
@@ -16,6 +16,10 @@ const Single = () => {
     const [matchedPairs, setMatchedPairs] = useState(0);
     const [moves, setMoves] = useState(0);
 
+    const [time, setTime] = useState(0);
+    const [isActive, setIsActive] = useState(true);
+    const [gameOver, setGameOver] = useState(false);
+
     const [menuOpen, setMenuOpen] = useState(false);
 
     const shuffleCards = () => {
@@ -54,6 +58,13 @@ const Single = () => {
         }
     }, [firstChoice, secondChoice]);
 
+    useEffect(() => {
+        if (matchedPairs === totalPairs) {
+            setIsActive(false);
+            setGameOver(true);
+        }
+    }, [matchedPairs, totalPairs]);
+
     const resetTurn = () => {
         setFirstChoice(null);
         setSecondChoice(null);
@@ -72,10 +83,34 @@ const Single = () => {
         setMenuOpen(false);
         setMatchedPairs(0);
         setMoves(0);
+        setTime(0);
+        setIsActive(true);
+        setGameOver(false);
     };
 
     const handleResumeGame = () => {
         setMenuOpen(false);
+    };
+
+    useEffect(() => {
+        let interval;
+        if (isActive) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 1);
+            }, 1000);
+        } else if (!isActive && time !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isActive, time]);
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+            2,
+            "0"
+        )}`;
     };
 
     return (
@@ -137,13 +172,61 @@ const Single = () => {
             <div className="w-full mt-auto flex justify-center gap-6 items-center">
                 <div className="h-[70px] w-[151px] bg-[#DFE7EC] rounded-[5px] flex flex-col items-center justify-center ">
                     <p className="littleBoxTitle text-[#7191A5]">Time</p>
-                    <p className="littleBoxValue text-[#304859]">00:00</p>
+                    <p className="littleBoxValue text-[#304859]">
+                        {formatTime(time)}
+                    </p>
                 </div>
                 <div className="h-[70px] w-[151px] bg-[#DFE7EC] rounded-[5px] flex flex-col items-center justify-center ">
                     <p className="littleBoxTitle text-[#7191A5]">Moves</p>
                     <p className="littleBoxValue text-[#304859]">{moves}</p>
                 </div>
             </div>
+            {gameOver && (
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center h-full w-full px-6">
+                    <div className="bg-white p-6 flex items-center justify-center gap-6 flex-col rounded-[10px] w-full">
+                        <div className="flex flex-col items-center gap-2">
+                            <h1 className="youDidIt text-[#152938]">
+                                You did it!
+                            </h1>
+                            <p className="youDidItP text-[#7191A5]">
+                                Game over! Here's how you got on…
+                            </p>
+                        </div>
+                        <div className="flex flex-col gap-2 w-full">
+                            <div className="w-full h-12 bg-[#DFE7EC] flex items-center justify-between p-4 rounded-[5px]">
+                                <p className="text-[#7191A5] timeElapsed">
+                                    Time Elapsed:{" "}
+                                </p>
+                                <p className="timeElapsed text-[20px] text-[#304859]">
+                                    {formatTime(time)}
+                                </p>
+                            </div>
+                            <div className="w-full h-12 bg-[#DFE7EC] flex items-center justify-between p-4 rounded-[5px]">
+                                <p className="text-[#7191A5] timeElapsed">
+                                    Moves Taken:{" "}
+                                </p>
+                                <p className="timeElapsed text-[20px] text-[#304859]">
+                                    {moves} Moves
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-4 w-full">
+                            <button
+                                className="menuButton bg-[#FDA214] text-white"
+                                onClick={handleRestart}
+                            >
+                                Restart
+                            </button>
+                            <button
+                                className="menuButton bg-[#DFE7EC] text-[#304859]"
+                                onClick={() => navigate("/")}
+                            >
+                                Setup New Game
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
